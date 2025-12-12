bcdb819cf26eb322d63c568ef2496c3b6a9d9b7c
adas7099 <adas7099@gmail.com>
2025-12-12 21:44:52 +0530

    UserException change

--- employee/src/main/java/com/example/jwt/employee/employee/Controller/UserController.java
+++ employee/src/main/java/com/example/jwt/employee/employee/Controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -51,7 +52,7 @@ public class UserController {
             );
         }
         catch (Exception e){
-            e.printStackTrace();
+            throw new UsernameNotFoundException("USerName not found");
         }
 
 
